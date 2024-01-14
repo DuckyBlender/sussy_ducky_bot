3cb66e566ee5c52489726263d67baef66eb33d5c
Alan Klas <alan@alanklas.com>
2024-01-14 15:21:20 +0100

    ADDED /LLAVA OMGGGG

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,84 @@
-use teloxide::{prelude::*, utils::command::BotCommands, RequestError};
+use base64::prelude::*;
+use log::{debug, info};
+use serde_json::{json, Value};
+use teloxide::{net::Download, prelude::*, RequestError};
+
 mod ollama;
 use ollama::*;
 
 #[tokio::main]
 async fn main() {
-    pretty_env_logger::init();
     dotenv::dotenv().ok();
+    pretty_env_logger::init();
     log::info!("Starting command bot...");
 
     let bot = Bot::from_env();
-    // Set the commands
-    bot.set_my_commands(Command::bot_commands())
-        .await
-        .expect("Failed to set commands");
 
-    Command::repl(bot, answer).await;
+    teloxide::repl(bot, handler).await;
 }
 
-#[derive(BotCommands, Clone)]
-#[command(
-    rename_rule = "lowercase",
-    description = "These commands are supported:"
-)]
-enum Command {
-    #[command(description = "Display this text")]
-    Help,
-    #[command(description = "Generate using Mistral 7B")]
-    Mistral(String),
-    #[command(description = "Alias for /mistral", prefix = "m")]
-    M(String),
+fn parse_command(msg: &Message) -> (Option<&str>, Option<&str>) {
+    let text = msg.text().unwrap_or("");
+    let mut iter = text.splitn(2, ' ');
+    let command = iter.next();
+    let args = iter.next();
+
+    (command, args)
 }
 
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    match cmd {
-        Command::Help => {
-            bot.send_message(msg.chat.id, Command::descriptions().to_string())
-                .await?
+fn parse_command_in_caption(msg: &Message) -> (Option<&str>, Option<&str>) {
+    let text = msg.caption().unwrap_or("");
+    let mut iter = text.splitn(2, ' ');
+    let command = iter.next();
+    let args = iter.next();
+
+    (command, args)
+}
+
+async fn handler(bot: Bot, msg: Message) -> ResponseResult<()> {
+    info!("Received message: {}", msg.text().unwrap_or(""));
+
+    // Check if the message is a message or an image with a caption
+    if msg.photo().is_some() && msg.caption().is_some() {
+        info!("Message is an image with a caption");
+        let (command, args) = parse_command_in_caption(&msg);
+        debug!("Command: {:?}, args: {:?}", command, args);
+        match command {
+            Some("/llava") | Some("/l") => {
+                let prompt = args.unwrap_or("").to_string();
+                debug!("Executing llava command with prompt: {}", prompt);
+                llava(bot, msg, prompt).await?;
+            }
+            _ => {}
         }
-        Command::Mistral(prompt) | Command::M(prompt) => mistral(bot, msg, prompt).await?,
-    };
+    } else if msg.text().is_some() {
+        info!("Message is a text message");
+        let (command, args) = parse_command(&msg);
+        match command {
+            Some("/mistral") | Some("/m") => {
+                let prompt = args.unwrap_or("").to_string();
+                debug!("Executing mistral command with prompt: {}", prompt);
+                mistral(bot, msg, prompt).await?;
+            }
+            _ => {}
+        }
+    } else {
+        info!("Message is not a text message nor an image with a caption");
+    }
 
     Ok(())
 }
 
 async fn mistral(bot: Bot, msg: Message, prompt: String) -> Result<Message, RequestError> {
+    info!("Starting mistral function");
     // If the prompt is empty, check if there is a reply
     let prompt = if prompt.is_empty() {
         if let Some(reply) = msg.reply_to_message() {
             reply.text().unwrap_or("").to_string()
         } else {
-            bot.send_message(msg.chat.id, "No prompt provided").await?;
+            bot.send_message(msg.chat.id, "No prompt provided")
+                .reply_to_message_id(msg.id)
+                .await?;
             return Ok(msg);
         }
     } else {
@@ -58,7 +87,9 @@ async fn mistral(bot: Bot, msg: Message, prompt: String) -> Result<Message, Requ
 
     // Check if prompt is nothing
     if prompt.is_empty() {
-        bot.send_message(msg.chat.id, "No prompt provided").await?;
+        bot.send_message(msg.chat.id, "No prompt provided")
+            .reply_to_message_id(msg.id)
+            .await?;
         return Ok(msg);
     }
 
@@ -79,9 +110,13 @@ async fn mistral(bot: Bot, msg: Message, prompt: String) -> Result<Message, Requ
         .await;
 
     match res {
-        Ok(_) => {}
+        Ok(_) => {
+            log::info!("Request sent successfully");
+        }
         Err(e) => {
+            log::debug!("Error sending request: {}", e);
             bot.send_message(msg.chat.id, format!("Error: {}", e))
+                .reply_to_message_id(msg.id)
                 .await?;
             return Ok(msg);
         }
@@ -98,9 +133,91 @@ async fn mistral(bot: Bot, msg: Message, prompt: String) -> Result<Message, Requ
                 .await
         }
         Err(e) => {
+            log::debug!("Error parsing response: {}", e);
             bot.send_message(msg.chat.id, format!("Error: {}", e))
                 .reply_to_message_id(msg.id)
                 .await
+        }
+    }
+}
+
+async fn llava(bot: Bot, msg: Message, mut prompt: String) -> Result<Message, RequestError> {
+    log::info!("Starting llava function");
+
+    log::info!("Prompt: {}", prompt);
+
+    if prompt.is_empty() {
+        prompt = "What is in this image?".to_string();
+    }
+
+    let photo = match msg.photo() {
+        Some(photos) => photos.last().unwrap(),
+        None => {
+            bot.send_message(msg.chat.id, "No image provided")
+                .reply_to_message_id(msg.id)
+                .await?;
+            return Ok(msg);
+        }
+    };
+
+    log::info!("Photo: {:?}", photo);
+
+    let file_path = bot.get_file(photo.file.id.clone()).await?.path;
+    let mut buf = Vec::new();
+    bot.download_file(&file_path, &mut buf).await?;
+
+    let base64_image = BASE64_STANDARD.encode(&buf);
+
+    bot.send_chat_action(msg.chat.id, teloxide::types::ChatAction::Typing)
+        .await?;
+
+    let request_body = &OllamaRequest {
+        model: "llava".to_string(),
+        prompt,
+        stream: false,
+        images: Some(vec![base64_image]),
+    };
+
+    // Save this request in json to the disk
+    let request_body_json = json!(request_body);
+    let request_body_json = serde_json::to_string_pretty(&request_body_json).unwrap();
+    use std::fs::File;
+    use std::io::Write;
+    let mut file = File::create("request.json").unwrap();
+    file.write_all(request_body_json.as_bytes()).unwrap();
+
+    // log::info!("Request body: {:?}", request_body);
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post("http://localhost:11434/api/generate")
+        .json(&request_body)
+        .send()
+        .await;
+
+    match response {
+        Ok(response) => {
+            let res: Value = response.json().await?;
+            // let text = response.text().await?;
+            if let Some(response_text) = res["response"].as_str() {
+                log::info!("Response text: {}", response_text);
+
+                bot.send_message(msg.chat.id, response_text)
+                    .reply_to_message_id(msg.id)
+                    .await
+            } else {
+                bot.send_message(msg.chat.id, "Error: no response")
+                    .reply_to_message_id(msg.id)
+                    .await
+            }
+        }
+        Err(e) => {
+            log::info!("Error sending request: {}", e);
+            bot.send_message(msg.chat.id, format!("Error: {}", e))
+                .reply_to_message_id(msg.id)
+                .await?;
+
+            Err(e.into())
         }
     }
 }
