a43509e3e9f1332fc3e487b0eaaad2c48eb63749
Alan Klas <alan@alanklas.com>
2024-02-11 13:05:16 +0100

    forgot to parse as html lmao

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -2,6 +2,7 @@ use teloxide::payloads::SendMessageSetters;
 use teloxide::prelude::Requester;
 use teloxide::requests::ResponseResult;
 
+use teloxide::types::ParseMode;
 use teloxide::{types::Message, Bot};
 
 use crate::Commands;
@@ -10,5 +11,6 @@ pub async fn help(bot: Bot, msg: Message) -> ResponseResult<Message> {
     let help_text = Commands::new().help_message();
     bot.send_message(msg.chat.id, help_text)
         .reply_to_message_id(msg.id)
+        .parse_mode(ParseMode::Html)
         .await
 }
