6aea078959a0345cee557bb45f246f7f4d6aec5d
Alan Klas <alan@alanklas.com>
2024-04-09 17:43:51 +0200

    send typing indicator and fix empty prompt

--- src/commands/ollama.rs
+++ src/commands/ollama.rs
@@ -23,6 +23,12 @@ pub async fn ollama(
 ) -> Result<(), RequestError> {
     info!("Starting ollama function");
 
+    if prompt.is_empty() {
+        bot.send_message(msg.chat.id, "Please provide a prompt")
+            .await?;
+        return Ok(());
+    }
+
     // Send a message to the chat to show that the bot is generating a response
     let generating_message = bot
         .send_message(msg.chat.id, "Generating response...")
@@ -35,10 +41,14 @@ pub async fn ollama(
         .await?;
 
     // Log the request (as JSON)
-    info!("Sending request to ollama using model {} and length: {}", model_type, prompt.len());
+    info!(
+        "Sending request to ollama using model {} and length: {}",
+        model_type,
+        prompt.len()
+    );
 
     // Send the stream request using ollama-rs
-    let before_request   = std::time::Instant::now();
+    let before_request = std::time::Instant::now();
 
     let ollama = Ollama::default();
     let request = GenerationRequest::new(model_type.to_string(), prompt);
@@ -47,8 +57,9 @@ pub async fn ollama(
     // Create a repeating interval that yields every 5 seconds
     let mut now = std::time::Instant::now();
 
-    // Create a string to hold the entire responseAppend [...] when the bot is still recieving 
+    // Create a string to hold the entire responseAppend [...] when the bot is still recieving
     let mut entire_response = String::new();
+    let mut current_string = String::new();
 
     // Parse the response and edit the message every 5 seconds
     loop {
@@ -57,16 +68,40 @@ pub async fn ollama(
             for ele in res {
                 // Append the new response to the entire response
                 entire_response.push_str(&ele.response);
-                
+
                 // Check if 5 seconds have passed since last edit
                 if now.elapsed().as_secs() >= INTERVAL_SEC {
+                    // Check if the message is identical. Don't know if this is necessary but it's here for now
+                    if current_string == entire_response {
+                        continue;
+                    }
+
+                    current_string = entire_response.clone();
+
+                    // If the message is last, force edit the message
+                    // Don't know if this actually works
+                    if ele.final_data.is_some() {
+                        info!("Final response received using method 1");
+                        bot.edit_message_text(
+                            generating_message.chat.id,
+                            generating_message.id,
+                            current_string.clone(),
+                        )
+                        .await?;
+                        break;
+                    }
+
                     // Edit the message
                     bot.edit_message_text(
                         generating_message.chat.id,
                         generating_message.id,
-                        entire_response.clone() + " [...]",
+                        current_string.clone() + " [...]",
                     )
                     .await?;
+
+                    // Send the typing indicator
+                    bot.send_chat_action(msg.chat.id, ChatAction::Typing)
+                        .await?;
 
                     // Reset the timer
                     now = std::time::Instant::now();
@@ -74,6 +109,7 @@ pub async fn ollama(
             }
         } else {
             // If the stream has no more responses, break the loop
+            info!("Final response received using method 2");
             bot.edit_message_text(
                 generating_message.chat.id,
                 generating_message.id,
