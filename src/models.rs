3e806c5d69d7ce155191d19bb4e3615ec5e8cf2c
Alan Klas <alan@alanklas.com>
2024-08-14 02:32:01 +0200

    removed MORE models

--- src/models.rs
+++ src/models.rs
@@ -8,13 +8,10 @@ pub enum ModelType {
     Uncensored,
     Caveman,
     Racist,
-    TinyLlama,
     Lobotomy,
-    StableLM2,
     Phi3,
     Json,
     PolishLobotomy,
-    TinyStories,
     Gemma2,
     GLM4,
 
@@ -22,7 +19,6 @@ pub enum ModelType {
     SDXLTurbo,
     SDXL,
     StableAudio,
-    AuraFlow,
     FluxShnell,
 
     // Perplexity (online)
@@ -35,14 +31,6 @@ pub enum ModelType {
     // OpenAI (best LLMs, paid)
     // GPT4o,
     GPT4oMini,
-    Dalle3,
-
-    // Bedrock
-    AmazonTitanText,
-    AmazonTitanTextLite,
-    AmazonTitanImage,
-    AmazonTitanImageVariation,
-    Claude3,
 }
 
 impl ModelType {
@@ -52,19 +40,15 @@ impl ModelType {
 
     pub fn return_ollama() -> Vec<ModelType> {
         vec![
-            ModelType::TinyLlama,
             ModelType::Lobotomy,
-            ModelType::StableLM2,
             ModelType::Uncensored,
             ModelType::Phi3,
             ModelType::Racist,
             ModelType::Json,
             ModelType::PolishLobotomy,
-            ModelType::TinyStories,
             ModelType::Caveman,
             ModelType::Gemma2,
             ModelType::GLM4,
-            ModelType::TinyStories,
             ModelType::Caveman,
         ]
     }
@@ -74,27 +58,16 @@ impl ModelType {
             ModelType::SDXLTurbo,
             ModelType::SDXL,
             ModelType::StableAudio,
-            ModelType::AuraFlow,
             ModelType::FluxShnell,
         ]
     }
 
     pub fn return_openai() -> Vec<ModelType> {
-        vec![ModelType::GPT4oMini, ModelType::Dalle3]
+        vec![ModelType::GPT4oMini]
     }
 
     pub fn return_perplexity() -> Vec<ModelType> {
         vec![ModelType::Online]
-    }
-
-    pub fn return_bedrock() -> Vec<ModelType> {
-        vec![
-            ModelType::AmazonTitanText,
-            ModelType::AmazonTitanTextLite,
-            ModelType::AmazonTitanImage,
-            ModelType::AmazonTitanImageVariation,
-            ModelType::Claude3,
-        ]
     }
 
     pub fn return_groq() -> Vec<ModelType> {
@@ -104,13 +77,7 @@ impl ModelType {
     pub fn owner_only() -> Vec<ModelType> {
         vec![
             ModelType::Online,
-            ModelType::AmazonTitanText,
-            ModelType::AmazonTitanTextLite,
-            ModelType::AmazonTitanImage,
-            ModelType::AmazonTitanImageVariation,
-            ModelType::Claude3,
             ModelType::StableAudio,
-            ModelType::AuraFlow,
         ]
     }
 }
@@ -118,33 +85,23 @@ impl ModelType {
 impl std::fmt::Display for ModelType {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            ModelType::PolishLobotomy => write!(f, "duckyblender/polish-lobotomy"), // for ollama
-            ModelType::TinyStories => write!(f, "duckyblender/tinystories-656k"),   // for ollama
-            ModelType::Json => write!(f, "phi3:3.8b-mini-4k-instruct-q4_K_M"),      // for ollama
-            ModelType::Phi3 => write!(f, "phi3:3.8b-mini-4k-instruct-q4_K_M"),      // for ollama
-            ModelType::GPT4oMini => write!(f, "gpt-4o-mini"),                       // for openai
-            ModelType::Uncensored => write!(f, "mannix/llama3.1-8b-abliterated"),   // for ollama
-            ModelType::LLAMA3 => write!(f, "llama-3.1-70b-versatile"),              // for groq
-            ModelType::Caveman => write!(f, "caveman-llama3"),                      // for ollama
-            ModelType::Racist => write!(f, "duckyblender/racist-phi3"),             // for ollama
-            ModelType::TinyLlama => write!(f, "tinyllama:1.1b-chat-v0.6-q8_0"),     // for ollama
-            ModelType::Lobotomy => write!(f, "qwen:0.5b-chat-v1.5-q2_K"),           // ollama
-            ModelType::Online => write!(f, "llama-3-sonar-large-32k-online"), // for perplexity.ai
-            ModelType::StableLM2 => write!(f, "stablelm2"),                   // for ollama
-            ModelType::Dalle3 => write!(f, "dall-e-3"),                       // for openai
-            ModelType::AmazonTitanText => write!(f, "amazon.titan-text-express-v1"), // for bedrock
-            ModelType::AmazonTitanTextLite => write!(f, "amazon.titan-text-lite-v1"), // for bedrock
-            ModelType::AmazonTitanImage => write!(f, "amazon.titan-image-generator-v1"), // for bedrock
-            ModelType::AmazonTitanImageVariation => write!(f, "amazon.titan-image-generator-v1"), // for bedrock
-            ModelType::Claude3 => write!(f, "anthropic.claude-3-5-sonnet-20240620-v1:0"), // for bedrock
-            ModelType::Gemma2 => write!(f, "gemma2:9b-instruct-q4_K_M"), // for ollama
-            ModelType::GLM4 => write!(f, "glm4:9b-chat-q4_K_M"),         // for ollama
-            ModelType::SDXLTurbo => write!(f, "fast-lightning-sdxl"),    // for fal.ai
-            ModelType::SDXL => write!(f, "fast-sdxl"),                   // for fal.ai
-            ModelType::StableAudio => write!(f, "stable-audio"),         // for fal.ai
-            ModelType::AuraFlow => write!(f, "aura-flow"),               // for fal.ai
-            ModelType::FluxShnell => write!(f, "flux/schnell"),          // for fal.ai
-            ModelType::Rushify => write!(f, "llama-3.1-8b-instant"),     // for groq
+            ModelType::PolishLobotomy => write!(f, "duckyblender/polish-lobotomy"),
+            ModelType::Json => write!(f, "phi3:3.8b-mini-4k-instruct-q4_K_M"),
+            ModelType::Phi3 => write!(f, "phi3:3.8b-mini-4k-instruct-q4_K_M"),
+            ModelType::GPT4oMini => write!(f, "gpt-4o-mini"),
+            ModelType::Uncensored => write!(f, "mannix/llama3.1-8b-abliterated"),
+            ModelType::LLAMA3 => write!(f, "llama-3.1-70b-versatile"),
+            ModelType::Caveman => write!(f, "caveman-llama3"),
+            ModelType::Racist => write!(f, "duckyblender/racist-phi3"),
+            ModelType::Lobotomy => write!(f, "qwen:0.5b-chat-v1.5-q2_K"),
+            ModelType::Online => write!(f, "llama-3-sonar-large-32k-online"),
+            ModelType::Gemma2 => write!(f, "gemma2:9b-instruct-q4_K_M"),
+            ModelType::GLM4 => write!(f, "glm4:9b-chat-q4_K_M"),
+            ModelType::SDXLTurbo => write!(f, "fast-lightning-sdxl"),
+            ModelType::SDXL => write!(f, "fast-sdxl"),
+            ModelType::StableAudio => write!(f, "stable-audio"),
+            ModelType::FluxShnell => write!(f, "flux/schnell"),
+            ModelType::Rushify => write!(f, "llama-3.1-8b-instant"),
         }
     }
 }
